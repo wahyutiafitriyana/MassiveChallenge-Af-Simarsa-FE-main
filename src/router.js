1033e93c39a5d6b0feee9faaeabe5476cae15bcb
Wahyu Tia Fitriyana <114914921+wahyutiafitriyana@users.noreply.github.com>
2023-01-07 12:55:58 +0700

    Add files via upload

---
+++ src/router.js
@@ -0,0 +1,39 @@
+const HOME = "/";
+const ABOUT = "/tentang-kami";
+const DOKTER = "/jadwal-dokter";
+const DAFTAR = "/pendaftaran";
+const LAYANAN = "/fasilitas";
+const HUBUNGI = "/hubungi-kami";
+const ADMIN_DASHBOARD = "/dashboard";
+const LOGIN = "/login";
+const DATA_DOKTER = "/data-dokter";
+const DETAIL_DOKTER = "/detail-dokter";
+const DATA_POLIKLINIK = "/data-poliklinik";
+const DATA_POLI = "/data-poli";
+const DATA_PASIEN = "/data-pasien";
+const RIWAYAT_PASIEN = "/riwayat-pasien";
+const DAFTAR_PASIEN_BARU = "/daftar-pasienbaru";
+const DAFTAR_BERHASIL = "/daftar-berhasil";
+const KARTU_ANTRIAN = "/kartu-antrian";
+const DAFTAR_PASIEN_LAMA = "/daftar-pasienlama"
+
+export {
+  HOME,
+  ABOUT,
+  DAFTAR,
+  DOKTER,
+  LAYANAN,
+  HUBUNGI,
+  ADMIN_DASHBOARD,
+  LOGIN,
+  DATA_DOKTER,
+  DETAIL_DOKTER,
+  DATA_POLIKLINIK,
+  DATA_POLI,
+  DATA_PASIEN,
+  RIWAYAT_PASIEN,
+  DAFTAR_PASIEN_BARU,
+  DAFTAR_BERHASIL,
+  KARTU_ANTRIAN,
+  DAFTAR_PASIEN_LAMA
+};
